8d50c6c7aa7644d9d0793694b73056edd44abe39
Uyathandwa Nkosi <Uyathandwa Nkosi@DESKTOP-JS1TDH1>
2024-04-10 09:21:20 +0200

    Add project files.

---
+++ ShopriteOnline/Controllers/CategoryController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopriteOnline.Data;
+using ShopriteOnline.Models;
+using System.Collections.Generic;
+
+namespace ShopriteOnline.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CategoryDbContext db;
+
+        public CategoryController(CategoryDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> category = db.categories.ToList();
+            return View(category);
+        }
+
+        //Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //Post for creating category
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //Custom Validation
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value.");
+            }
+            if (ModelState.IsValid)
+            {
+                db.categories.Add(obj);
+                db.SaveChanges();
+                //return View();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        //Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categpryFromDb = db.categories.Find(id);
+            if (categpryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categpryFromDb);
+        }
+        //Edit Post
+        //Update
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categories.Update(obj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+
+        //Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categpryFromDb = db.categories.Find(id);
+            if (categpryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categpryFromDb);
+        }
+        //Post Delete
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = db.categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            db.categories.Remove(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
---
+++ ShopriteOnline/Data/CategoryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopriteOnline.Models;
+
+namespace ShopriteOnline.Data
+{
+    public class CategoryDbContext : DbContext
+    {
+        public CategoryDbContext(DbContextOptions<CategoryDbContext>options):
+            base(options)
+        {
+         
+        }
+        //DbSet means Table name Categories
+        public DbSet<Category> categories { get; set; }
+        //Add the Seed
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+               new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+               new Category { Id = 2, Name = "Food", DisplayOrder = 2 },
+               new Category { Id = 3, Name = "History", DisplayOrder = 3 });
+        }
+
+
+    }
+}
